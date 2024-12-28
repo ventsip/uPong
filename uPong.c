2afca2d5d43a3369dccd79b99f333d7f94ff890f
8-10 <ventsip@gmail.com>
2024-12-28 21:08:37 +0200

    Refactor code formatting and enable USB output for improved functionality

--- uPong.c
+++ uPong.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/dma.h"
 #include "hardware/pio.h"
 #include "hardware/timer.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 // Data will be copied from src to dst
 const char src[] = "Hello, world! (from DMA)";
@@ -10,7 +10,8 @@ char dst[count_of(src)];
 
 #include "blink.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq)
+{
     blink_program_init(pio, sm, offset, pin);
     pio_sm_set_enabled(pio, sm, true);
 
@@ -21,12 +22,11 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
     pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 }
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
+int64_t alarm_callback(alarm_id_t id, void *user_data)
+{
     // Put your timeout handler code in here
     return 0;
 }
-
-
 
 int main()
 {
@@ -34,16 +34,16 @@ int main()
 
     // Get a free channel, panic() if there are none
     int chan = dma_claim_unused_channel(true);
-    
+
     // 8 bit transfers. Both read and write address increment after each
     // transfer (each pointing to a location in src or dst respectively).
     // No DREQ is selected, so the DMA transfers as fast as it can.
-    
+
     dma_channel_config c = dma_channel_get_default_config(chan);
     channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
     channel_config_set_read_increment(&c, true);
     channel_config_set_write_increment(&c, true);
-    
+
     dma_channel_configure(
         chan,          // Channel to be configured
         &c,            // The configuration we just created
@@ -52,12 +52,12 @@ int main()
         count_of(src), // Number of transfers; in this case each is 1 byte.
         true           // Start immediately.
     );
-    
+
     // We could choose to go and do something else whilst the DMA is doing its
     // thing. In this case the processor has nothing else to do, so we just
     // wait for the DMA to finish.
     dma_channel_wait_for_finish_blocking(chan);
-    
+
     // The DMA has now copied our text from the transmit buffer (src) to the
     // receive buffer (dst), so we can print it out from there.
     puts(dst);
@@ -66,19 +66,20 @@ int main()
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &blink_program);
     printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
+
+#ifdef PICO_DEFAULT_LED_PIN
     blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
+#else
     blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
+#endif
     // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
 
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
     // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
