f414b4799e3f3ecb22a5ba6a16e120afa202af44
8-10 <ventsip@gmail.com>
2024-12-29 16:41:55 +0200

    Add unit tests for colors_to_bitplanes function and update LED configuration

--- src/uPong.c
+++ src/uPong.c
@@ -15,8 +15,8 @@
 #error Attempting to use a pin>=32 on a platform that does not support it
 #endif
 
-#define LEDS_PER_STRIP 10
-#define NMB_STRIPS 2 // max 8 strips
+#define LEDS_PER_STRIP (256 * 2)
+#define NMB_STRIPS 3 // max 8 strips
 #if NMB_STRIPS > 8
 #error "NMB_STRIPS must be <= 8"
 #endif
@@ -26,6 +26,36 @@ typedef uint8_t bit_plane_type; // must be wide enough to contain the number of 
 
 #define BYTES_PER_LED 3
 
+void colors_to_bitplanes_standard(
+    bit_plane_type *const bitplane,
+    const uint8_t *const colors,
+    const int nmb_strips,
+    const int leds_per_strip,
+    const int bytes_per_led)
+{
+    memset(bitplane, 0, leds_per_strip * bytes_per_led * 8);
+
+    int color_byte_offset = 0;
+    for (int y = 0; y < nmb_strips; y++)
+    {
+        for (int x = 0; x < leds_per_strip; x++)
+        {
+            for (int c = 0; c < bytes_per_led; c++, color_byte_offset++)
+            {
+                // iterate over color bits
+                for (int i = 0; i < 8; i++)
+                {
+                    const uint8_t bit = (colors[color_byte_offset] >> (7 - i)) & 1;
+                    if (bit)
+                    {
+                        const int bitplanes_byte_offset = (x * bytes_per_led + c) * 8 + i;
+                        bitplane[bitplanes_byte_offset] |= (1 << y);
+                    }
+                }
+            }
+        }
+    }
+}
 // two bit planes, each consists of LEDS_PER_STRIP * BYTES_PER_LED elements of bit_plane_type
 // bit planes are effectivley a transposed version of the color values of each led of each strip
 // the two bit planes are used for double buffering
@@ -163,9 +193,32 @@ void set_color(uint8_t *colors, uint8_t r, uint8_t g, uint8_t b)
     colors[2] = b;
 }
 
+int unit_tests()
+{
+    const int nmb_strips = NMB_STRIPS, leds_per_strip = LEDS_PER_STRIP, bytes_per_led = BYTES_PER_LED;
+
+    uint8_t colors[nmb_strips * leds_per_strip * bytes_per_led];
+    // initialize colors with random values
+    for (int i = 0; i < nmb_strips * leds_per_strip * bytes_per_led; i++)
+    {
+        colors[i] = rand() % 256;
+    }
+    bit_plane_type bitplane_s[leds_per_strip * bytes_per_led * 8];
+    colors_to_bitplanes_standard(bitplane_s, colors, nmb_strips, leds_per_strip, bytes_per_led);
+
+    bit_plane_type bitplane[leds_per_strip * bytes_per_led * 8];
+    colors_to_bitplanes(bitplane, colors, nmb_strips, leds_per_strip, bytes_per_led);
+
+    // compare the two bit planes
+    int ret = memcmp(bitplane_s, bitplane, leds_per_strip * bytes_per_led * 8);
+    return ret == 0;
+}
+
 int main()
 {
     stdio_init_all();
+
+    int tests = unit_tests();
 
     PIO pio;
     uint sm;
@@ -174,7 +227,6 @@ int main()
     // PIO Blinking LED
     success = pio_claim_free_sm_and_add_program_for_gpio_range(&blink_program, &pio, &sm, &offset, PICO_DEFAULT_LED_PIN, 1, true);
     hard_assert(success);
-    printf("Loaded program at %d\n", offset);
     blink_pin_forever(pio, sm, offset, PICO_DEFAULT_LED_PIN, 2);
 
     // This will find a free pio and state machine for our program and load it for us
@@ -187,16 +239,30 @@ int main()
     sem_init(&reset_delay_complete_sem, 1, 1); // initially posted so we don't block first time
     dma_init(pio, sm);
 
-    int active_planes = 0;
+    int frame_buffer_index = 0;
     int color = 0;
     int brightness = 0;
+    int frame = 0;
+    int frame_rate = 0;
+    absolute_time_t last_time = {0};
     while (true)
     {
-        // int red = (color >= 0 && color < 256) ? brightness : 0;
-        // int green = (color >= 256 && color < 256 + 256) ? brightness : 0;
-        // int blue = (color >= 256 + 256 && color < 256 + 256 + 256) ? brightness : 0;
-        // color = (color + 1) % 256 + 256 + 256;
-        // brightness = (brightness + 1) % 256;
+        // calculate fps
+        frame++;
+
+        absolute_time_t current_time = get_absolute_time();
+        if (absolute_time_diff_us(last_time, current_time) >= 1000000)
+        {
+            frame_rate = frame;
+            frame = 0;
+            last_time = current_time;
+        }
+
+        int red = (color >= 0 && color < 256) ? brightness : 0;
+        int green = (color >= 256 && color < 256 + 256) ? brightness : 0;
+        int blue = (color >= 256 + 256 && color < 256 + 256 + 256) ? brightness : 0;
+        color = (color + 1) % (256 + 256 + 256);
+        brightness = (brightness + 1) % 256;
 
         for (int y = 0; y < NMB_STRIPS; y++)
         {
@@ -208,27 +274,30 @@ int main()
                 }
                 else
                 {
-                    set_color(&led_colors[(y * LEDS_PER_STRIP + x) * BYTES_PER_LED], 22, 22, 22);
+                    set_color(&led_colors[(y * LEDS_PER_STRIP + x) * BYTES_PER_LED], red, green, blue);
                 }
             }
         }
-        // print color
-        // printf("Color: R:%d G:%d  B:%d\n", red, green, blue);
 
         // convert the colors to bit planes
         absolute_time_t start_time = get_absolute_time();
-        colors_to_bitplanes(led_strips_bitplanes[active_planes], led_colors, NMB_STRIPS, LEDS_PER_STRIP, BYTES_PER_LED);
+        colors_to_bitplanes(led_strips_bitplanes[frame_buffer_index], led_colors, NMB_STRIPS, LEDS_PER_STRIP, BYTES_PER_LED);
         absolute_time_t end_time = get_absolute_time();
-        int64_t time_diff_us = absolute_time_diff_us(start_time, end_time);
-        printf("colors_to_bitplanes took %lld us\n", time_diff_us);
+        int64_t time_color_to_bitplanes = absolute_time_diff_us(start_time, end_time);
 
+        start_time = get_absolute_time();
         sem_acquire_blocking(&reset_delay_complete_sem);
-        output_colors_dma(active_planes);
-        // toggle active planes
-        active_planes ^= 1;
+        end_time = get_absolute_time();
+        int64_t time_wait_for_DMA = absolute_time_diff_us(start_time, end_time);
 
-        // printf("WS2812 parallel using pin %d\n", WS2812_PIN_BASE);
-        // printf("Loaded ws2812 program at pio %d, sm %d, offset %d\n", pio, sm, offset);
-        sleep_ms(10);
+        output_colors_dma(frame_buffer_index);
+        // toggle active planes
+        frame_buffer_index ^= 1;
+
+        printf("FPS %d; ", frame_rate);
+        printf("unit tests %s; ", tests ? "passed" : "failed");
+        printf("R:%03d G:%03d  B:%03d; ", red, green, blue);
+        printf("colors_to_bitplanes: %lld us; ", time_color_to_bitplanes);
+        printf("waited DMA to finish %lld us\n", time_wait_for_DMA);
     }
 }
